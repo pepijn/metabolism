eb1da8259eec9644c94053f4551a4c80693de3fe
Pepijn Looije <pepijnlooije@gmail.com>
2011-12-05 21:46:23 +0100

    Deployable

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+# Stel de applicatie in
+set :application, 'rivm'
+
+require 'capistrano_colors'
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'rvm/capistrano'
+require "bundler/capistrano"
+
+set :user, 'deployer'
+set :use_sudo, false
+
+server 'plict.nl', :app, :web, :db, :primary => true
+
+set :deploy_to, "/var/www/#{application}"
+set :repository, "gitosis@plict.nl:#{application}.git"
+set :scm, "git"
+set :branch, "master"
+
+namespace :deploy do
+  desc "Restart application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+namespace :log do
+  desc "Tail the production log"
+  task :default do
+    run "tail -n 300 -f #{current_path}/log/production.log"
+  end
+end
+
+require './config/boot'
